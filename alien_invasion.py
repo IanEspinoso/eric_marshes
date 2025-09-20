bb6f99819db5c849e1cf77ab4b83a283aeca3a7e
Espinoso <espinoso.ian@gmail.com>
2025-09-20 10:45:53 -0300

    Modularized game settings and created a gitignore to include temporary fields

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """General class to manage actions and behaviors within the game"""
@@ -7,8 +8,11 @@ class AlienInvasion:
     def __init__(self):
         """Initializes the game and creates resources for it"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -19,8 +23,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # Redraws the screen with each loop
+            self.screen.fill(self.settings.bg_color)
+
             # Leaves visible the most recently drawn screen
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Instantiates and executes the game
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Class to store the game configurations for Alien Invasion"""
+
+    def __init__(self):
+        """Initializes the game configurations"""
+        # Screen configurations
+        self.screen_width = 1200
+        self.screen_height= 800
+        self.bg_color = (230, 230, 230)
