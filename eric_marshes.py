481d929e69f3ebf69111dbf3e96a0f7a1e288588
IanEspinoso <espinoso.ian@gmail.com>
2025-10-05 21:32:38 -0300

    Implemented game in full screen

--- eric_marshes.py
+++ eric_marshes.py
@@ -9,14 +9,16 @@ class EricMarshes:
     def __init__(self):
         """Initializes the game and creates resources for it"""
         pygame.init()
-        self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Eric Marshes")
 
         self.sertanejo = Sertanejo(self)
+
+        self.clock = pygame.time.Clock()
     
 
     def run_game(self):
