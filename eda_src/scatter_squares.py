cabf5cdf1a0ed8c552ac362a0f5a1c69329aa885
IanEspinoso <espinoso.ian@gmail.com>
2025-11-02 14:03:43 -0300

    Stylized the curve, and save the image in the cwd

--- eda_src/scatter_squares.py
+++ eda_src/scatter_squares.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
+import os
+
 
 x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Define the graph's title and axes labels
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,5 +19,6 @@ ax.tick_params(labelsize=14)
 
 # Defines the interval of each axis
 ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='plain')
 
-plt.show()
+plt.('squares_plot.png', bbox_inches='tight')
