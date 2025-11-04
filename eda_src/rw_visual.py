e270f1f443591813ec60fbc88fb4a93cadaa571f
IanEspinoso <espinoso.ian@gmail.com>
2025-11-03 21:29:36 -0300

    Update the walk plotter to include 50k steps

--- eda_src/rw_visual.py
+++ eda_src/rw_visual.py
@@ -5,19 +5,19 @@ from random_walk import RandomWalk
 # Keeps creating new walks, while the program remains active
 while True:
     # Calls a random walk
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
-
     # Plots the walk's steps
     plt.style.use('classic')
     fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
-               edgecolors='none', s=15)
+               edgecolors='none', s=1)
     ax.set_aspect('equal')
 
     # Highlights the first and last elements
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c-'red', edgecolors='none', 
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', 
                s=100)
 
     plt.show()
