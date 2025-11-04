d3fc49c5e5fc27358d83ae84e5b52c15a01c92ad
IanEspinoso <espinoso.ian@gmail.com>
2025-11-03 21:20:20 -0300

    Update the walk plotter to include the option of further iterations

--- eda_src/rw_visual.py
+++ eda_src/rw_visual.py
@@ -2,13 +2,19 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Calls a random walk
-rw = RandomWalk()
-rw.fill_walk()
+# Keeps creating new walks, while the program remains active
+while True:
+    # Calls a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plots the walk's steps
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    # Plots the walk's steps
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
