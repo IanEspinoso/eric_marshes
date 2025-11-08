b32fe596e23566620b8199b807a7075df9127076
IanEspinoso <espinoso.ian@gmail.com>
2025-11-08 13:41:04 -0300

    Create visuals with plotly

--- eda_src/die_visual.py
+++ eda_src/die_visual.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+
 from die import Die
 
 # Creates a D6
@@ -16,5 +18,8 @@ for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
-
+# Plots the results
+title = "Results of Rolling One D6 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
