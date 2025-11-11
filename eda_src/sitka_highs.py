0885ccb327c32bac0cf0506c7c6abb311a02a4df
IanEspinoso <espinoso.ian@gmail.com>
2025-11-10 23:29:58 -0300

    Plotting data in a temperature chart

--- eda_src/sitka_highs.py
+++ eda_src/sitka_highs.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt
+
 path = Path('./data/chapter_16/the_csv_file_format/weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
 
@@ -13,4 +15,15 @@ for row in reader:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+# Plots the maximum temperatures
+plt.style.use('fast')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Formats the chart
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
