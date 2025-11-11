8f55d47017f266382263a1028ecc6f9bc29642a0
IanEspinoso <espinoso.ian@gmail.com>
2025-11-10 23:19:49 -0300

    Parse the CSV headers

---
+++ eda_src/sitka_highs.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import csv
+
+path = Path('./data/chapter_16/the_csv_file_format/weather_data/sitka_weather_07-2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+print(header_row)
