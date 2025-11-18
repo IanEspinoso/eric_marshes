63461cc825b5bcdc4762b04b1a70310b2fc31dd3
IanEspinoso <espinoso.ian@gmail.com>
2025-11-17 21:53:26 -0300

    Color-code the spots by magnitude

--- eda_src/eq_explore_data.py
+++ eda_src/eq_explore_data.py
@@ -21,5 +21,10 @@ for eq_dict in  all_eq_dicts:
     lats.append(lat)
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+                     color=mags,
+                     color_continuous_scale='viridis',
+                     labels={'color':'Magnitude'},
+                     projection='natural earth',
+                     )
 fig.show()
