8f73bdda1dbebd80252c44a18a54b86c1008b520
IanEspinoso <espinoso.ian@gmail.com>
2025-11-17 21:37:38 -0300

    Creates a list with all the earthquakes

--- eda_src/eq_explore_data.py
+++ eda_src/eq_explore_data.py
@@ -6,7 +6,6 @@ path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents = path.read_text(encoding="utf-8")
 all_eq_data = json.loads(contents)
 
-# Creates a more readable version of the data archive
-path = Path('eq_data/readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
+# Examines all earthquakes in the dataset
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))                           
