578ebf5f5142e67b9529eb6f96b9747c637582d8
IanEspinoso <espinoso.ian@gmail.com>
2025-11-23 22:03:22 -0300

    Explore the returned dictionary

--- eda_src/python_repos.py
+++ eda_src/python_repos.py
@@ -10,6 +10,15 @@ print(f"Status code: {r.status_code}")
 
 # Converts the response object into a dictionary
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# Processes the results
-print(response_dict.keys())
+# Explores information on the repositories
+repo_dicts = response_dict['items']
+print(f"repositories returned: {len(repo_dicts)}")
+
+# Examines the first repository
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
