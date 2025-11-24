3c612da7b572c7d2c9566cfeeda58d558a93708f
IanEspinoso <espinoso.ian@gmail.com>
2025-11-23 21:59:55 -0300

    Create a Github API call and response processor

---
+++ eda_src/python_repos.py
@@ -0,0 +1,15 @@
+import requests
+
+# Creates an API call and verifies the response
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept":"application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Converts the response object into a dictionary
+response_dict = r.json()
+
+# Processes the results
+print(response_dict.keys())
