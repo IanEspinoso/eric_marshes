03d9097823054938989670a0ab03860e900a7b07
IanEspinoso <espinoso.ian@gmail.com>
2025-11-28 22:25:21 -0300

    Defined an Entry model

--- django_ui_src/learning_log/learning_logs/models.py
+++ django_ui_src/learning_log/learning_logs/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 class Topic(models.Model):
-    """A topic that the user is learning"""
+    """ A topic that the user is learning """
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """ Returns a string representation of the model """
         return self.text
+    
+class Entry(models.Model):
+    """ Something specific learned about a topic """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+        def __str__(self):
+            """ Returns a simple string to represent the entry """
+            return f"{self.text[:50]}..."
